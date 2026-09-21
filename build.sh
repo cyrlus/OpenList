031c216ea968bf8c32082823727cbc47d7086745
cyrlus <40169274+cyrlus@users.noreply.github.com>
2026-09-21 15:55:50 +0800

    Refactor build script to use environment variables
    Signed-off-by: cyrlus <40169274+cyrlus@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 set -e
-set -x 
 appName="openlist"
 builtAt="$(date +'%F %T %z')"
-gitAuthor="The OpenList Projects Contributors <noreply@openlist.team>"
-gitCommit=$(git log --pretty=format:"%h" -1)
+gitAuthor="${GIT_AUTHOR:-OpenList Projects Contributors <noreply@openlist.team>}"
+gitCommit="${GIT_COMMIT:-unknown}"
 
 # Set frontend repository, default to OpenListTeam/OpenList-Frontend
 frontendRepo="${FRONTEND_REPO:-OpenListTeam/OpenList-Frontend}"
@@ -26,10 +25,8 @@ elif [ "$1" = "beta" ]; then
   version="beta"
   webVersion="rolling"
 else
-  git tag -d beta || true
-  # Always true if there's no tag
-  version=$(git describe --abbrev=0 --tags 2>/dev/null || echo "v0.0.0")
-  webVersion=$(eval "curl -fsSL --max-time 2 $githubAuthArgs \"https://api.github.com/repos/$frontendRepo/releases/latest\"" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+  version="${VERSION:-v0.0.0}"
+  webVersion="${WEB_VERSION:-$(eval "curl -fsSL --max-time 2 $githubAuthArgs \"https://api.github.com/repos/$frontendRepo/releases/latest\"" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')}"
 fi
 
 echo "backend version: $version"
